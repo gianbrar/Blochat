03bdf3f2e3fe6fbbb225d1b0fa686562b250f416
BuildTools <unconfigured@null.spigotmc.org>
2020-11-07 17:12:15 -0500

    Visual Changes

--- blochat/visual.py
+++ blochat/visual.py
@@ -24,7 +24,7 @@ def launchserver():
 
 window = Tk()
 window.title("Blochat")
-window.configure(background="black")
+window.configure(background='#1D3557')
 def invalidip(err):
   if sys.platform.startswith("linux"):
     playsound.playsound("CTX/err.wav")
@@ -33,7 +33,7 @@ def invalidip(err):
   Label(
     window,
     text=err,
-    bg="black",
+    bg="#1D3557",
     fg="red",
     font="none 14 bold").grid(
         row=4, column=0, sticky=N)
@@ -41,25 +41,25 @@ Button(
     window, text="Server Mode", width=10, command=launchserver).grid(
         row=0, column=10, sticky=W)
 photo1 = PhotoImage(file="CTX/blochat_icon_small.png")
-Label(window, image=photo1, bg="black").grid(row=1, column=0, sticky=W)
+Label(window, image=photo1, bg="#1D3557").grid(row=1, column=0, sticky=W)
 
 Label(
-    window, text="Blochat", bg="black", fg="white", font="none 60 bold").grid(
+    window, text="Blochat", bg="#1D3557", fg="white", font="none 60 bold").grid(
         row=1, column=1, sticky=S)
 
 Label(
     window,
     text="\n\nEnter Server IP In\nThe Box Below",
-    bg="black",
+    bg="#1D3557",
     fg="white",
     font="none 20 bold").grid(
         row=2, column=0, sticky=N)
 
 tBg = ""
 if sys.platform.startswith("linux"):
-  tBg = "black"
+  tBg = "#457B9D"
 else:
-  tBg = "white"
+  tBg = "#A8DADC"
 
 textbloch = Entry(window, width=40, bg=tBg)
 textbloch.grid(row=3, column=0, sticky=N)
